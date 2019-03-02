708608c65f4218a2df70f1ef789bdf102187719c
Rickard Jarheim <rickard.jarheim@setpace.se>
2019-03-02 20:35:54 +0100

    First commit

---
+++ src/main/java/stuff/LittlePageObject.java
@@ -0,0 +1,19 @@
+package stuff;
+
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@Component
+public class LittlePageObject {
+    @Autowired
+    protected WebDriver driver;
+
+    public void justTesting() {
+        driver.manage().window().maximize();
+    }
+}
---
+++ src/test/java/integration/GoogleTest.java
@@ -0,0 +1,31 @@
+package integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import stuff.LittlePageObject;
+import stuff.WebContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = WebContext.class)
+@ComponentScan(basePackages = "stuff")
+@EnableAutoConfiguration
+public class GoogleTest {
+    @Autowired
+    private WebDriver driver;
+
+    @Autowired
+    private LittlePageObject littlePageObject;
+
+    @Test
+    public void visitGoogle() {
+        driver.get("https://www.google.se/");
+
+        littlePageObject.justTesting();
+    }
+}
